c4a40768fa381c29e9c84ceb9d47cd66c73406be
Bruno Leonardo <135679874+brunols7@users.noreply.github.com>
2025-03-01 23:49:42 -0300

    Dicee App

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,75 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(const MainApp());
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text(
+            'Dicee', 
+            style: TextStyle(
+              color: Colors.white, 
+              fontWeight: FontWeight.bold
+            ),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class DicePage extends StatefulWidget {
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  void randomizeDice(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            child: OutlinedButton(
+              child: Image.asset('assets/images/dice$leftDiceNumber.png'),
+              onPressed: (){
+                randomizeDice();
+              },
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(color: Colors.transparent)
+              ),
+            )
+          ),
+          Expanded(
+            child: OutlinedButton(
+              child: Image.asset('assets/images/dice$rightDiceNumber.png'),
+              onPressed: (){
+                randomizeDice();
+              },
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(color: Colors.transparent)
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
